30bf22ceb5c7fef90b2820a4eda9d71f80d64609
chang <changrui0608>
2017-05-10 14:42:33 +0800

    web:group.py

--- web/tomato/admin/group.py
+++ web/tomato/admin/group.py
@@ -4,29 +4,60 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
 from . import AddEditForm, RemoveConfirmForm
-from tomato.crispy_forms.layout import Layout
+from ..crispy_forms.layout import Layout
 from ..lib import wrap_rpc
 from ..admin_common import Buttons
 
 
 class GroupForm(AddEditForm):
-	# TODO
-	pass
+
+	name = forms.CharField(max_length=64, label="Name", help_text="The group's name. Must be unique.")
+	label = forms.CharField(max_length=255, label="Label", help_text="")
+	description = forms.CharField(widget=forms.Textarea, label="Description", required=False)
+
+	buttons = Buttons.cancel_add
+
+	def __init__(self, *args, **kwargs):
+		super(GroupForm, self).__init__(*args, **kwargs)
+		self.helper.layout = Layout(
+			'name',
+			'label',
+			'description',
+			self.buttons
+		)
+
+	def get_redirect_after(self):
+		return HttpResponseRedirect(reverse("admin_group_info", kwargs={"name": self.cleaned_data['name']}))
 
 
 class AddGroupForm(GroupForm):
-	# TODO
-	pass
+	title = 'Add group'
+
+	def __init__(self, *args, **kwargs):
+		super(AddGroupForm, self).__init__(*args, **kwargs)
+
+	def submit(self, api):
+		formData = self.get_optimized_data()
+		api.group_create(formData)
 
 
 class EditGroupForm(GroupForm):
-	# TODO
-	pass
+	title = "Editing group %(name)s"
+	buttons = Buttons.cancel_save
+
+	def __init__(self, *args, **kwargs):
+		super(EditGroupForm, self).__init__(*args, **kwargs)
+		self.fields["name"].widget = forms.TextInput(attrs={'readonly': 'readonly'})
+		self.fields["name"].help_text = None
+
+	def submit(self, api):
+		formData = self.get_optimized_data()
+		api.group_modify(formData['name'], {k: v for k, v in formData.iteritems() if k not in ('name',)})
 
 
 class RemoveGroupForm(RemoveConfirmForm):
-	# TODO
-	pass
+	message = "Are you sure you want to remove the group '%(name)s'?"
+	title = "Remove Group '%(name)s'"
 
 
 @wrap_rpc
@@ -51,12 +82,40 @@ def info(api, request, group):
 	return render(request, "group/info.html", {"group": group, "role": role})
 
 
+@wrap_rpc
+def add(api, request):
+	if request.method == 'POST':
+		form = AddGroupForm(data=request.POST)
+		if form.is_valid():
+			form.submit(api)
+			return form.get_redirect_after()
+		else:
+			return form.create_response(request)
+	else:
+		form = AddGroupForm()
+		return form.create_response(request)
+
 
 @wrap_rpc
 def edit(api, request, group):
-	# TODO
-	pass
+	if request.method == 'POST':
+		form = EditGroupForm(data=request.POST)
+		if form.is_valid():
+			form.submit(api)
+			return form.get_redirect_after()
+		else:
+			return form.create_response(request)
+	else:
+		form = EditGroupForm(data=api.group_info(group))
+		return form.create_response(request)
 
 
-
-
+@wrap_rpc
+def remove(api, request, group):
+	if request.method == 'POST':
+		form = RemoveGroupForm(name=group, data=request.POST)
+		if form.is_valid():
+			api.group_remove(group)
+			return HttpResponseRedirect(reverse('admin_group_list'))
+	form = RemoveGroupForm(name=group)
+	return form.create_response(request)
