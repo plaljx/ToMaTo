01126617a13247629327777de8ce30ddf73081b7
caihhh <nongcaihua@foxmail.com>
2017-06-23 15:57:19 +0800

    traffic backend

--- backend_core/tomato/api/traffic.py
+++ backend_core/tomato/api/traffic.py
@@ -33,56 +33,7 @@ def traffic_start(element_id, traffic_ids):
         res = traffic.Traffic.traffic_start(element_id, traffic_id)
     return res
 
-'''
-
-import os
-import requests
-from .elements import element_action, element_info
-action = "rextfv_upload_grant"
-action_use = "rextfv_upload_use"
-
-
-def traffic_start(element_id, trafficIds):
-	for trafficId in trafficIds:
-		pack_dir = make_pack(trafficId)
-		send_result=send_pack(element_id, pack_dir)
-		action_result = element_action(element_id,action_use)
-	return action_result
-
-def get_element(id):
-	info = element_info(id)
-	return info
-
-def get_send_key(id, action, params={}):
-	res = element_action(id, action, params)
+def ditg_start(element_id, attrs=None):
+	print attrs
+	res = traffic.ditg_start(element_id , **attrs)
 	return res
-
-def make_pack(traffic_id):
-	traffic_info = _getTraffic(traffic_id)
-	if os.path.exists("/work/%s/" % traffic_id):
-		os.system("rm  /work/%s/*" % traffic_id)
-	else:
-		os.mkdir("/work/%s" % traffic_id)
-	on_event = "0.0 ON 1 UDP DST %s/%s %s" % (traffic_info.dest_ip , traffic_info.dest_port , traffic_info.pattern)
-	print "pattern" + on_event + "\n"
-	off_event  = "20.0 OFF 1"
-	f = open("/work/%s/auto_exec.sh" % traffic_id, "w")
-	f.write('#!/bin/bash' + '\n' + 'mgen event \"%s\" event \"%s\"' % (on_event , off_event))
-	f.close()
-	os.chdir("/work/%s/" % traffic_id)
-	os.system("tar czvf %s.tar.gz auto_exec.sh" % traffic_id)
-	#return "/work/%s.tar.gz" % name
-	return "/work/%s/%s.tar.gz" % (traffic_id , traffic_id)
-
-def send_pack(element_id , pack_dir):
-	info = get_element(element_id)
-	key = get_send_key(element_id, action)
-	url = "http://" + str(info["host_info"]["address"]) + ":" + str(info["host_info"]["fileserver_port"]) + "/" + key + "/upload"
-
-	if os.path.exists(pack_dir):
-		print pack_dir + "exist"
-	upload = {"file":open(pack_dir, "rb")}
-	r = requests.post(url, files=upload)
-	print r.text
-    return r.text
-'''
--- backend_core/tomato/lib/traffic.py
+++ backend_core/tomato/lib/traffic.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 
-def make_pack(traffic_info):
+def make_mgen_pack(traffic_info):
 	if not os.path.exists("/work"):
 		os.mkdir("/work")
 	if os.path.exists("/work/%s/" % traffic_info["id"]):
@@ -29,3 +29,26 @@ def send_pack(element_info , pack_dir, key):
 	r = requests.post(url, files=upload)
 	print r.text
 	return r.text
+
+def make_ditg_pack(event ,element_id ,  **attrs):
+	if not os.path.exists("/ditg"):
+		os.mkdir("/ditg")
+	if os.path.exists("/ditg/%s/" % element_id):
+		os.system("rm /ditg/%s/*" % element_id)
+	else:
+		os.mkdir("/ditg/%s" % element_id)
+	cmd = ""
+	if event == "receive":
+		cmd = "/usr/share/D-ITG-2.8.1-r1023/bin/ITGRecv"
+	if event == "send":
+		cmd = "/usr/share/D-ITG-2.8.1-r1023/bin/ITGSend -T %s -a 10.0.0.3 -c 512 -C 100 -t %s -l sender.log -x receiver.log " \
+			  %(attrs["protocol"] ,  attrs["duration"])
+		print cmd
+	f = open("/ditg/%s/auto_exec.sh" % element_id, "w")
+	f.write('#!/bin/bash' + '\n' + '%s' % cmd)
+	f.close()
+	os.chdir("/ditg/%s/" % element_id)
+	os.system("tar czvf %s.tar.gz auto_exec.sh" % element_id)
+	# return "/work/%s.tar.gz" % name
+	return "/ditg/%s/%s.tar.gz" % (element_id, element_id)
+
