de3b6a9cfc3b7a1b3b7daea343d67c0eb007c10a
nongcaihua <nongcaihua@meituan.com>
2018-03-13 18:11:46 +0800

    fix

--- web/tomato/js/editor/component.js
+++ web/tomato/js/editor/component.js
@@ -419,8 +419,8 @@ var Component = Class.extend({
 		this.ditgWindow.show();
 	},
 	ipToId:function(ip){ //change the input ip to element's id
-		for(var i = 0 ; i < this.compoent.topology.data.elements.length; i++){
-			var order = this.compoent.topology.elements[i].data
+		for(var i = 0 ; i < this.topology.data.elements.length; i++){
+			var order = this.topology.elements[i].data
 			if(order.tech == "openvz_interface"){
 				if(order.ip4address == ip||order.ip4address == (ip+"/24")||order.ip4address == (ip+"/22")){
 					return order.parent;
